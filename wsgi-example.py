8325bfbbc010da17d5924e056ae34ba66831d7f5
Alex Recker <alex@reckerfamily.com>
2014-04-04 12:37:33 -0500

    Added example wsgi script

---
+++ wsgi-example.py
@@ -0,0 +1,10 @@
+import os, sys
+sys.path.append('/PATH/TO/PROJECT/FILES')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Blog.settings'
+activate_this = "/PATH/TO/VIRTUALENV/bin/activate_this.py"
+try:
+    execfile(activate_this, dict(__file__=activate_this))
+except:
+    pass
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
