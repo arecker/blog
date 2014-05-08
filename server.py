c87ad7151b5711c80122cade122394d6c0af9a46
Alex Recker <alex@reckerfamily.com>
2014-05-08 11:12:40 -0500

    Archives page and smaller server.py

--- server.py
+++ server.py
@@ -12,24 +12,6 @@ def GetHome():
     return home_page
 
 
-@app.route("/archives/")
-def GetArchives():
-    archives_page = open(join(config.cache, 'archives.html'), 'r').read()
-    return archives_page
-
-
-@app.route("/projects/")
-def GetProjects():
-    projects_page = open(join(config.cache, 'projects.html'), 'r').read()
-    return projects_page
-
-
-@app.route("/friends/")
-def GetFriends():
-    friends_page = open(join(config.cache, 'friends.html'), 'r').read()
-    return friends_page
-
-
 @app.route("/sitemap.xml")
 def GetSiteMap():
     xml = open(join(config.static, 'sitemap.xml'), 'r').read()
