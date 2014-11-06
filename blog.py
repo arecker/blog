4f31794b341e57ca7c82dcc4d57d78d0e8884ec9
Alex Recker <alex@reckerfamily.com>
2014-11-06 17:02:14 -0600

    Scaffolding out the commands.

--- blog.py
+++ blog.py
@@ -14,5 +14,61 @@ def cli_refresh():
     pass
 
 
+@cli.command(name="serve")
+def cli_serve():
+    """
+    serve site locally
+    """
+    pass
+
+
+@cli.command(name="deploy")
+def cli_deploy():
+    """
+    sync server's html cache with project's
+    """
+    pass
+
+
+@cli.group(name="mail")
+def cli_mail():
+    """
+    manage email subscription engine
+    """
+    pass
+
+
+@cli_mail.command(name="list")
+def cli_mail_list():
+    """
+    list current subscribers
+    """
+    pass
+
+
+@cli_mail.command(name="add")
+def cli_mail_add():
+    """
+    add a subscriber
+    """
+    pass
+
+
+@cli_mail.command(name="remove")
+def cli_mail_remove():
+    """
+    remove a subscriber
+    """
+    pass
+
+
+@cli_mail.command(name="latest")
+def cli_mail_latest():
+    """
+    send latest post to subscribers
+    """
+    pass
+
+
 if __name__ == '__main__':
     cli()
