c42a2c375a2d1ad75dc80e60c1aa6649838a15ff
Alex Recker <alex@reckerfamily.com>
2015-01-06 22:46:33 -0600

    Travis: going to try cloning posts repo

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,3 +10,14 @@ class TestConfig(unittest.TestCase):
     def test_read_config(self):
         config = core.Config()
         self.assertIsNotNone(config)
+
+
+
+class TestPost(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+    def test_get_all_posts(self):
+        list = core.Post.get_all_posts()
+        self.assertEquals(len(list), len(os.listdir(core.Config().posts)))
