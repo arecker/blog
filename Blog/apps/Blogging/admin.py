dfe281fccd5af91a065a8799ac91fb41dce29bd9
Alex Recker <alex@reckerfamily.com>
2015-02-02 20:45:55 -0600

    Rewrite: adds more stuff to Post admin

--- Blog/apps/Blogging/admin.py
+++ Blog/apps/Blogging/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'get_truncated_description', 'published')
+    list_filter = ('published',)
+    search_fields = ('title', 'description')
+
+
+admin.site.register(Post, PostAdmin)
--- Blog/apps/Blogging/models.py
+++ Blog/apps/Blogging/models.py
@@ -1,10 +1,29 @@
 from django.db import models
+import slugify
 
 
 class Post(models.Model):
     title = models.CharField(verbose_name='Title', max_length=120)
-    slug = models.SlugField(verbose_name='Slug', max_length=120, unique=True)
+    slug = models.SlugField(verbose_name='Slug', max_length=120, unique=True, blank=True)
     date = models.DateField(verbose_name='Date', auto_created=True, db_index=True)
     published = models.BooleanField(verbose_name='Publish', default=False)
+    description = models.TextField(verbose_name='Description', blank=True, null=True)
     body = models.TextField(verbose_name='Body', blank=True, null=True)
 
+
+    def save(self, *args, **kwargs):
+        # Extend save function to slugify title
+        self.slug = slugify.slugify(self.title, to_lower=True)
+        return super(Post, self).save(*args, **kwargs)
+
+
+    def get_truncated_description(self, length=50, suffix='...'):
+        if len(self.description) <= length:
+            return self.description
+        else:
+            return ' '.join(self.description[:length+1].split(' ')[0:-1]) + suffix
+    get_truncated_description.short_description = 'Description'
+
+
+    def __unicode__(self):
+        return self.title
