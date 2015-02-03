c49134e5255bd8402e39268a4a54d3faec0e28bb
Alex Recker <alex@reckerfamily.com>
2015-02-02 19:57:08 -0600

    Rewrite: wired up post model with admin

--- Blog/apps/Blogging/models.py
+++ Blog/apps/Blogging/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=120)
+    slug = models.SlugField(verbose_name='Slug', max_length=120, unique=True)
+    date = models.DateField(verbose_name='Date', auto_created=True, db_index=True)
+    published = models.BooleanField(verbose_name='Publish', default=False)
+    body = models.TextField(verbose_name='Body', blank=True, null=True)
+
