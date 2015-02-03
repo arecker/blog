fe5c84ecda4703da88701bcfa849169054d01ce0
Alex Recker <alex@reckerfamily.com>
2015-02-02 19:56:53 -0600

    Rewrite: added settings to path of manage script

--- scripts/dev_manage.py
+++ scripts/dev_manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+add_me = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(add_me)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blog.settings.development")
 
