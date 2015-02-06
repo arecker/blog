217efaaf3b9b298eaa02361120f4cf185a78fc5c
Alex Recker <alex@reckerfamily.com>
2015-02-06 17:41:06 -0600

    Adds home view to url/views

--- Blog/apps/Blogging/views.py
+++ Blog/apps/Blogging/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+def get_home(request):
+    return render_to_response("common/base.html")
