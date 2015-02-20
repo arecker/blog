4f79f0bd529333fe55540c95483fd9a8a0425f58
Alex Recker <alex@reckerfamily.com>
2015-02-19 20:22:00 -0600

    Fixed routing bug.

--- Blog/api_urls.py
+++ Blog/api_urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url, include
 
-urlpatterns = patterns('Blog.apps.Subscribing.views',
+urlpatterns = patterns('',
     url(r'^subscribing/', include('Blog.apps.Subscribing.api_urls')),
     url(r'^blogging/', include('Blog.apps.Blogging.api_urls')),
 )
--- Blog/apps/Blogging/api_urls.py
+++ Blog/apps/Blogging/api_urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('Blog.apps.Blogging.api_views.',
+urlpatterns = patterns('Blog.apps.Blogging.api_views',
     url(r'^post/$', 'get_posts', name='home'),
 )
