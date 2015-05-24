d3f469b0ea12375099f26f8f1df5f8123df9ceff
Alex Recker <alex@reckerfamily.com>
2015-05-24 18:14:39 -0500

    JSHINT

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,23 @@
 module.exports = function(grunt){
     grunt.initConfig({
-
+	jshint: {
+	    all: [
+		'./static/js/app.js',
+		'./*/static/*/scripts/*'
+	    ],
+	    options: {
+		curly: true,
+		eqeqeq: true,
+		eqnull: true,
+		camelcase: true,
+		forin: true,
+		funcscope: true,
+		latedef: true,
+		maxparams: 5,
+		futurehostile: true
+	    }
+	}
     });
-    grunt.registerTask('default', []);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
 };
