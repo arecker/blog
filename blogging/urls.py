1f7946f64d113a9c2d89b6b416d989063f8c9920
Alex Recker <alex@reckerfamily.com>
2015-05-29 17:20:50 -0500

    Bugfix: Bad import statement

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url, include
 from . import views
-from . import api
 
 
 urlpatterns = [
     url(r'^feed/$', views.view_post_feed),
     url(r'^(?P<slug>[^/]+)/$', views.view_post)
-    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
