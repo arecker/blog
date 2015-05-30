8898ec87889098aa499889194136267aafccf06c
Alex Recker <alex@reckerfamily.com>
2015-05-29 23:13:01 -0500

    Newsletter

--- subscribing/admin.py
+++ subscribing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Subscriber
+from .models import Subscriber, PostNewsletter
 
 
 admin.site.register(Subscriber)
+admin.site.register(PostNewsletter)
--- subscribing/models.py
+++ subscribing/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from blogging.models import Post
+from .email import PostEmail
 import uuid
+import logging
+logger = logging.getLogger(__name__)
 
 
 class Subscriber(models.Model):
@@ -11,3 +15,22 @@ class Subscriber(models.Model):
 
     def __unicode__(self):
         return self.email
+
+
+class PostNewsletter(models.Model):
+    post = models.ForeignKey(Post)
+    timestamp = models.DateTimeField(auto_created=True)
+    send_on_save = models.BooleanField(default=False, verbose_name='Send on Save')
+
+
+    def __unicode__(self):
+        return self.post.title
+
+
+    def save(self):
+        if self.send_on_save:
+            for sub in Subscriber.objects.all():
+                PostEmail(sub, self.post).send()
+                self.send_on_save = False
+                logging.info('PostNewsletter {0} sent to {1}'.format(self.post.title, sub.email))
+        super(PostNewsletter, self).save()
