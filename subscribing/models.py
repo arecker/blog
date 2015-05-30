1432d415616e514edfb437ea490f976801db897f
Alex Recker <alex@reckerfamily.com>
2015-05-30 13:52:39 -0500

    Mail exception handling

--- subscribing/models.py
+++ subscribing/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from blogging.models import Post
 from .email import PostEmail
 import uuid
+import logging
 
 
 class Subscriber(models.Model):
@@ -28,13 +29,20 @@ class PostNewsletter(models.Model):
     def save(self):
         if self.send_on_save:
             for sub in Subscriber.objects.all():
-                PostEmail(sub, self.post).send()
-                self.send_on_save = False
-                self._log(sub)
+                try:
+                    PostEmail(sub, self.post).send()
+                    self.send_on_save = False
+                    self._log(sub)
+                except Exception as e:
+                    self._error(sub, e)
         super(PostNewsletter, self).save()
 
 
     def _log(self, sub):
-        import logging
         logger = logging.getLogger()
         logger.info('PostNewsletter {0} sent to {1}'.format(self.post.title, sub.email))
+
+
+    def _error(self, sub, e):
+        logger = logging.getLogger()
+        logger.info('Error sending {0} to {1}: {2}'.format(self.post.title, sub.email, e))
