0c4a1c209281d3484eb6cf4b1afa7cb0345ec4c5
Alex Recker <alex@reckerfamily.com>
2015-06-21 23:33:35 -0500

    Coding: Added created date and model ordering

--- coding/models.py
+++ coding/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Project(models.Model):
@@ -6,8 +7,12 @@ class Project(models.Model):
     description = models.TextField(max_length=200, blank=True, null=True)
     image = models.ImageField(upload_to='coding/project', null=True, blank=True)
     url = models.URLField(verbose_name='URL', blank=True, null=True)
+    added = models.DateField(default=timezone.now)
 
 
     def __unicode__(self):
         return self.name
-    
+
+
+    class Meta:
+        ordering = ['-added']
