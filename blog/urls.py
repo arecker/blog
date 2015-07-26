9bc055f4d52d403962bd4fe528e9bf89d3ae36b7
Alex Recker <alex@reckerfamily.com>
2015-07-26 12:51:59 -0500

    Adds robots.txt

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic.simple import direct_to_template
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -8,6 +9,13 @@ from subscribing.views import get_unsubscribe_view
 
 
 urlpatterns = [
+    url(
+        r'^robots\.txt$', direct_to_template,
+        {
+            'template': 'robots.txt',
+            'mimetype': 'text/plain'
+        }
+    ),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
     url(r'^unsubscribe/(?P<key>[^/]+)/$', get_unsubscribe_view),
