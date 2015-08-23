87c5115aa083ad746451e313b78dfdefad75ee51
Alex Recker <alex@reckerfamily.com>
2015-08-23 10:52:37 -0500

    Renames theme js file to custom.js

---
+++ static/js/custom.js
@@ -0,0 +1,39 @@
+/*
+  This was hacked out of the clean blog theme code
+  I didn't need all the shitty validation stuff
+  TODO: rewrite this
+ */
+// Navigation Scripts to Show Header on Scroll-Up
+jQuery(document).ready(function($) {
+    var MQL = 1170;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.navbar-custom').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function() {
+                var currentTop = $(window).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-visible');
+                    } else {
+                        $('.navbar-custom').removeClass('is-visible is-fixed');
+                    }
+                } else {
+                    //if scrolling down...
+                    $('.navbar-custom').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                }
+                this.previousTop = currentTop;
+            });
+    }
+
+    $(".lazy").lazyload({
+        effect : "fadeIn"
+    });
+
+});
