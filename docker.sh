dd832dbefed92470cfc13a84ee4b60833ca8e5b3
Alex Recker <alex@reckerfamily.com>
2016-04-13 19:31:49 -0500

    Debuggin

--- docker.sh
+++ docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd /srv/src
+
+echo "In docker.sh" >> /srv/logs/blog.log
+echo "DB_USER: $DB_USER" >> /srv/logs/blog.log
+echo "DB_PASS: $DB_PASS" >> /srv/logs/blog.log
+echo "PWD: $(pwd)" >> /srv/logs/blog.log
+
 python manage.py migrate --settings=blog.settings.prod
 python manage.py collectstatic --noinput --settings=blog.settings.prod
 
