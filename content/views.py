0590c0083cd7a7e823018784785c8d207fffe771
Alex Recker <arecker@healthgrades.com>
2016-07-03 14:08:48 -0500

    Tweaks view

--- content/views.py
+++ content/views.py
@@ -7,27 +7,7 @@ from content.models import FortuneCookie, Image
 from serializers import FortuneCookieSerializer
 
 
-class BaseViewSetMixin(object):
-    @decorators.list_route(methods=['get'], url_path='random')
-    def random(self, request):
-        item = self.get_queryset().pluck()
-        if not item:
-            raise NotFound
-        serialzed_item = self.get_serializer(item)
-        return Response(serialzed_item.data)
-
-    @decorators.list_route(methods=['get'])
-    def slug(self, request):
-        query = request.query_params.get('q', None)
-        try:
-            item =  self.get_queryset().slug(slug=query)
-            serialzed_item = self.get_serializer(item)
-            return Response(serialzed_item.data)
-        except self.queryset.model.DoesNotExist:
-            raise NotFound
-
-
-class FortuneCookieViewSet(BaseViewSetMixin, viewsets.ReadOnlyModelViewSet):
+class FortuneCookieViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = FortuneCookie.objects.all()
     serializer_class = FortuneCookieSerializer
 
@@ -36,7 +16,7 @@ def random_image(request):
     image = Image.objects.pluck()
 
     if not image:
-        return Http404()
+        raise Http404('No images uploaded')
 
     with open(image.file.path) as file:
         return HttpResponse(file.read(), content_type='image/png')
