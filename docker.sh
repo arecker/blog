0d29d0098786f160f6db24e63a2d07fa7d6d99c1
Alex Recker <alex@reckerfamily.com>
2016-07-06 16:33:34 -0500

    Removes fanciness

--- docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /srv/src
-python manage.py migrate --settings=blog.settings.prod
-python manage.py collectstatic --noinput --settings=blog.settings.prod
-exec supervisord -c /srv/src/configs/supervisord.conf
