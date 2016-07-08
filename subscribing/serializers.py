dbf9a7d6944b957a1e80e8581269a6fe2c8adf0f
Alex Recker <alex@reckerfamily.com>
2016-07-08 00:09:30 -0500

    Adds custom create method

--- subscribing/serializers.py
+++ subscribing/serializers.py
@@ -4,6 +4,11 @@ from models import Subscriber
 
 
 class SubscriberSerializer(serializers.ModelSerializer):
+    def create(self, *args):
+        instance = super(SubscriberSerializer, self).create(*args)
+        instance.send_verify_email()
+        return instance
+
     class Meta:
         model = Subscriber
         fields = ('email',
