9b6b6aa5498b2bd75ea83a082f8b0c20e0678759
Alex Recker <alex@reckerfamily.com>
2018-03-31 18:52:49 -0500

    Uhh Yeah Dude

---
+++ blog.el
@@ -0,0 +1,27 @@
+(require 'ox-html)
+
+(setq org-src-fontify-natively t)
+
+(setq org-html-htmlize-output-type 'inline-css)
+
+(setq org-publish-project-alist '(("blog-html"
+				   :base-directory "~/git/blog/www"
+				   :base-extension "org"
+				   :html-head-extra "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />"
+				   :html-postamble nil
+				   :publishing-directory "~/git/blog/site"
+				   :publishing-function org-html-publish-to-html
+				   :recursive t
+				   :section-numbers nil
+				   :with-toc nil
+				   )
+				  ("blog-static"
+				   :base-directory "~/git/blog/www"
+				   :base-extension "css\\|jpg\\|jpeg\\|gif\\|png\\|txt\\|ogg"
+				   :publishing-directory "~/git/blog/site"
+				   :publishing-function org-publish-attachment
+				   :recursive t
+				   )
+
+				  ("blog" :components ("blog-html" "blog-static"))
+				  ))
