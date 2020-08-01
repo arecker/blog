17560e82de4c20afe03a5775e9060ef7cd3b60b3
Alex Recker <alex@reckerfamily.com>
2020-08-01 14:44:36 -0500

    AHHH

--- spec/tags_spec.rb
+++ spec/tags_spec.rb
@@ -5,6 +5,6 @@ require 'spec_helper.rb'
 describe JekyllRecker::Tags::Version do
   it 'should render the gem version' do
     rendered = Liquid::Template.parse('{% version %}').render
-    expect(rendered).to eq "v#{}"
+    expect(rendered).to eq "v#{JekyllRecker::VERSION}"
   end
 end
