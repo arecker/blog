d970a15566019371a44bc4ab72d275c870f79e0e
Alex Recker <alex@reckerfamily.com>
2020-08-02 11:49:41 -0500

    Post

--- new/blog.rb
+++ new/blog.rb
@@ -357,6 +357,43 @@ module Blog
     end
   end
 
+  # ArgParser
+  class ArgParser
+    def banner
+      "Usage: blog.rb <build>"
+    end
+
+    def options
+      @options ||= Struct.new(:name).new
+    end
+
+    def parser
+      @parser ||= OptionParser.new do |opts|
+        opts.banner = banner
+      end
+    end
+    # def self.parse(options)
+    # Options = Struct.new(:name)
+    #   args = Options.new("world")
+
+    #   opt_parser = OptionParser.new do |opts|
+    #     opts.banner = "Usage: example.rb [options]"
+
+    #     opts.on("-nNAME", "--name=NAME", "Name to say hello to") do |n|
+    #       args.name = n
+    #     end
+
+    #     opts.on("-h", "--help", "Prints this help") do
+    #       puts opts
+    #       exit
+    #     end
+    #   end
+
+    #   opt_parser.parse!(options)
+    #   return args      
+    # end
+  end
+
   def self.run!
     Builder.new.build!
     Rack::Handler::Thin.run(
