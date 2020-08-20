1e05c3fb08b258798717d428aebc237472af5ff2
Alex Recker <alex@reckerfamily.com>
2020-08-19 23:26:27 -0500

    Page

--- rnd/blog/pages.py
+++ rnd/blog/pages.py
@@ -1,4 +1,4 @@
-from . import files
+from . import files, markdown
 from .logging import logger
 
 
@@ -14,8 +14,16 @@ class Page:
     def href(self):
         return files.href(self.src)
 
+    @property
+    def is_markdown(self):
+        return os.path.splitext(self.src) == '.md'
+
     def build(self):
         logger.debug('building %s -> %s', self.src, self.href)
+
+    def render(self):
+        # if self.is_markdown():
+        pass
 
 
 class Entry(Page):
