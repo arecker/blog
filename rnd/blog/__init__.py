9c4da3aefc397ecb686c0a26c63a63c816060286
Alex Recker <alex@reckerfamily.com>
2020-08-29 20:51:23 -0500

    Get rid of RND stuff

--- rnd/blog/__init__.py
+++
@@ -1,4 +0,0 @@
-from .files import pave
-from .builders import build
-from .logging import enable_logger, logger
-from .options import parse_options
--- rnd/blog/meta.py
+++
@@ -1,8 +0,0 @@
-def descendants(cls):
-    subclasses = []
-
-    for subclass in cls.__subclasses__():
-        subclasses.append(subclass)
-        subclasses.extend(descendants(subclass))
-
-    return subclasses
