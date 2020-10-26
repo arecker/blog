f4c075e3c37ea5519f3ebccad65957f752a1bf7c
Alex Recker <alex@reckerfamily.com>
2020-10-25 21:43:15 -0500

    Add script

---
+++ scripts/replace_figures.py
@@ -0,0 +1,51 @@
+import os
+import re
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+r_figure = re.compile(
+    r'''{% include figure\.html\s+filename=['"]?(?P<filename>\S*)['"]\s+%}''',
+    flags=re.DOTALL
+)
+r_figure_with_caption = re.compile(
+    r'''{% include figure\.html\s+filename=['"]?(?P<filename>\S*)['"]\s+caption=['"]?(?P<caption>.*?)['"]?\s+%}''',
+    flags=re.DOTALL
+)
+
+
+def entries():
+    entries_dir = os.path.abspath(os.path.join(here, '../entries'))
+    return [os.path.join(entries_dir, entry) for entry in os.listdir(entries_dir)]
+
+
+def sub_figure(match):
+    src = '/images/' + match.group('filename')
+    alt = re.sub(r'[-_/]', ' ', os.path.splitext(match.group('filename'))[0])
+    html = '''
+<figure>
+  <a href="{src}">
+    <img alt="{alt}" src="{src}"/>
+  </a>
+</figure>'''.format(alt=alt, src=src).strip()
+    return html
+
+
+def main():
+    for entry in entries():
+        with open(entry) as f:
+            before = f.read()
+
+        after = r_figure.sub(sub_figure, before)
+        if before == after:
+            print('no change: {}'.format(entry))
+        else:
+            print('change   : {}'.format(entry))
+
+        with open(entry, 'w') as f:
+            f.write(after)
+
+
+
+if __name__ == '__main__':
+    main()
