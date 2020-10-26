6462af93151fc58e4606ded11b902a409192922b
Alex Recker <alex@reckerfamily.com>
2020-10-25 21:54:47 -0500

    Script

--- scripts/replace_figures.py
+++ scripts/replace_figures.py
@@ -36,12 +36,29 @@ def sub_figure(match):
     return html
 
 
+def sub_figure_with_caption(match):
+    src = '/images/' + match.group('filename')
+    alt = re.sub(r'[-_/]', ' ', os.path.splitext(match.group('filename'))[0])
+    caption = match.group('caption')
+    html = '''
+<figure>
+  <a href="{src}">
+    <img alt="{alt}" src="{src}"/>
+  </a>
+  <figcaption>
+    <p>{caption}</p>
+  </figcaption>
+</figure>'''.format(alt=alt, src=src, caption=caption).strip()
+    return html
+
+
 def main():
     for entry in entries() + old_posts():
         with open(entry) as f:
             before = f.read()
 
         after = r_figure.sub(sub_figure, before)
+        after = r_figure_with_caption.sub(sub_figure_with_caption, after)
         if before == after:
             print('no change: {}'.format(entry))
         else:
