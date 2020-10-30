6f4bc31d9265956eeef7476e3b521eba3727b407
Alex Recker <alex@reckerfamily.com>
2020-10-29 22:57:00 -0500

    Rebooting everything

--- scripts/entry_filter.lua
+++
@@ -1,8 +0,0 @@
-local entries = pandoc.pipe("ls", {}, "../entries")
-
-function Meta(m)
-   m.subtitle = m.title
-   m.date = os.date("%B %e, %Y")
-   m.title = os.date("%A, %B %d %Y")
-   return m
-end
