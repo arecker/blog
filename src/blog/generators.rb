eea874e5d3d7b73e3f7a0129c0142a5d30391032
Alex Recker <alex@reckerfamily.com>
2020-11-28 20:50:27 -0600

    Remove HTML validator generator

--- src/blog/generators.rb
+++ src/blog/generators.rb
@@ -153,16 +153,5 @@ module Blog
         site.data['coverage'] = JSON.parse(File.read(data_file))
       end
     end
-
-    # MarkupValidationGenerator
-    class MarkupValidationGenerator < Jekyll::Generator
-      include Base
-      include Blog::Shell
-
-      def generate(_site)
-        info 'validating HTML'
-        shell('rake html')
-      end
-    end
   end
 end
