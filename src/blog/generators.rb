3f260262574a7abdbc4533e30a8ab0f681baa0be
Alex Recker <alex@reckerfamily.com>
2020-12-27 15:38:35 -0600

    Stub out git and stats in dev mode

--- src/blog/generators.rb
+++ src/blog/generators.rb
@@ -21,17 +21,32 @@ module Blog
 
       def generate(site)
         @site = Site.new(site)
-        info 'calculating statistics'
-        site.data['stats'] = stats
+        site.data['stats'] = if @site.production?
+                               info 'calculating statistics'
+                               real_stats
+                             else
+                               info 'stubbing out statistics'
+                               stub_stats
+                             end
       end
 
-      def stats
-        @stats ||= {
+      def real_stats
+        {
           'total_words' => total(site.word_counts),
           'average_words' => average(site.word_counts).round(0),
           'total_posts' => site.entries.size,
           'consecutive_posts' => consecutive_posts,
           'swears' => calculate_swears
+        }
+      end
+
+      def stub_stats
+        {
+          'total_words' => 0,
+          'average_words' => 0,
+          'total_posts' => 0,
+          'consecutive_posts' => 0,
+          'swears' => 0
         }
       end
 
@@ -65,16 +80,39 @@ module Blog
       include Base
       include Blog::Git
 
+      attr_reader :site
+
       def generate(site)
-        info 'reading git history'
-        site.data.merge!(
-          {
-            'git_head' => git_head,
-            'git_head_summary' => git_head_summary,
-            'git_shorthead' => git_short_head,
-            'git_commit_count' => git_commit_count
-          }
-        )
+        @site = Site.new(site)
+        site.data.merge!(data)
+      end
+
+      def data
+        if site.production?
+          info 'reading git history'
+          real_data
+        else
+          info 'stubbing out git history'
+          stub_data
+        end
+      end
+
+      def real_data
+        {
+          'git_head' => git_head,
+          'git_head_summary' => git_head_summary,
+          'git_shorthead' => git_short_head,
+          'git_commit_count' => git_commit_count
+        }
+      end
+
+      def stub_data
+        {
+          'git_head' => '',
+          'git_head_summary' => '',
+          'git_shorthead' => '',
+          'git_commit_count' => 0
+        }
       end
     end
 
