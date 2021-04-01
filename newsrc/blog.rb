38aa3e15c011dce8891fd398420ba0d774e53ced
Alex Recker <alex@reckerfamily.com>
2021-04-01 00:43:42 -0500

    Fix comment in newsrc

--- newsrc/blog.rb
+++ newsrc/blog.rb
@@ -2,7 +2,7 @@
 
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
-# The official source code library of _alex-recker-dot-com_!
+# The official source code library of alex-recker-dot-com!
 module Blog
   autoload :Files, 'blog/files.rb'
   autoload :Logging, 'blog/logging.rb'
