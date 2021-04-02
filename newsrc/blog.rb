e9d27687cad70f00c68058484ab0b7f2a7d3a9e3
Alex Recker <alex@reckerfamily.com>
2021-04-01 23:02:43 -0500

    More render script

--- newsrc/blog.rb
+++ newsrc/blog.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 
 # The official source code library of alex-recker-dot-com!
 module Blog
+  autoload :Arguments, 'blog/arguments.rb'
   autoload :Files, 'blog/files.rb'
   autoload :Logging, 'blog/logging.rb'
 end
