8f0d5aab4dde77b320ef8564e249c649b6d4b0d9
Alex Recker <alex@reckerfamily.com>
2021-06-15 08:18:47 -0500

    R&D: add docstrings

--- src/lib/git.rb
+++ src/lib/git.rb
@@ -2,14 +2,17 @@
 
 # Functions for working with version control.
 module Git
+  # Returns the current HEAD
   def self.head
     `git rev-parse HEAD`.chomp
   end
 
+  # Returns the current git HEAD, only the short version.
   def self.short_head
     `git rev-parse --short HEAD`.chomp
   end
 
+  # Returns the commit message summary for the current HEAD.
   def self.head_summary
     `git log -1 --pretty=format:%s #{head}`
   end
