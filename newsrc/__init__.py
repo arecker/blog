c66eb1a1ee570db2ddc4caa54e0e2ac969b4a63f
Alex Recker <alex@reckerfamily.com>
2021-07-10 18:10:32 +0000

    R&D: move to absolute imports

--- newsrc/__init__.py
+++ newsrc/__init__.py
@@ -1,10 +1,10 @@
-from .cli import main, command
-from .config import config
-from .debug import launch_console
-from .entry import entries
-from .files import join, root, target, whatever_type_by_file
-from .logger import logger
-from .page import pages, make_global_context
-from .serve import serve
-from .test import run_tests
-from .version import version, python_version, python_executable
+from newsrc.cli import main, command
+from newsrc.config import config
+from newsrc.debug import launch_console
+from newsrc.entry import entries
+from newsrc.files import join, root, target, whatever_type_by_file
+from newsrc.logger import logger
+from newsrc.page import pages, make_global_context
+from newsrc.serve import serve
+from newsrc.test import run_tests
+from newsrc.version import version, python_version, python_executable
