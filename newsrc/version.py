1fbdc1fbfc31456eded0e85610aaa5bdc77fa064
Alex Recker <alex@reckerfamily.com>
2021-07-19 01:29:21 +0000

    R&D: delete old source

--- newsrc/version.py
+++
@@ -1,6 +0,0 @@
-from newsrc import files
-
-
-def version_string():
-    with open(files.join('src/VERSION')) as f:
-        return f.read().strip()
--- newsrc/yaml.py
+++
@@ -1,8 +0,0 @@
-import re
-
-line_pattern = re.compile(r'^(?P<key>[A-Za-z0-9]*)\s*:\s*(?P<value>.*)$', re.MULTILINE)
-
-
-def parse_yaml(content):
-    matches = line_pattern.findall(content)
-    return dict(matches)
