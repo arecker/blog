7ded2caa1c49aaea956d828d7fbf1bdcd1f33836
Alex Recker <alex@reckerfamily.com>
2022-02-05 14:43:01 -0600

    More R&D on index page.

--- src/commands/index.py
+++ src/commands/index.py
@@ -1,159 +1,21 @@
 """generate the site homepage"""
 
-import datetime
 import logging
-import re
 
-from .. import html, utils, git
-from .pages import Page, build_nav_list
-from .entries import Entry
+from .. import Document
 
 logger = logging.getLogger(__name__)
 
 
-class Index(Page):
-    filename = 'index.html'
-
-    # TODO: move these to a config
-    title = 'Dear Journal'
-    description = 'Daily, public journal by Alex Recker'
-
-    def read(self):
-        row = html.row()
-        latest = self.fetch_latest_entry()
-        column = self.html_latest_post(
-            title=latest.title,
-            description=latest.description,
-            page_href=f'./{latest.filename}',
-            banner_href=latest.banner_href()
-        )
-        row.append(column)
-
-        commit = git.get_head_commit()
-        column = self.html_last_updated(commit=commit, timestamp=self.make_timestamp())
-        row.append(column)
-        
-        row = html.stringify_xml(row)
-        row = self.fix_self_closing_tags(row)
-        return row
-
-    def html_latest_post(self, title='', description='', page_href='', banner_href=''):
-        """Render latest post column.
-
-        >>> kwargs = {'title': 'Test', 'description': 'a test'}
-        >>> kwargs.update({'page_href': 'test.html', 'banner_href': 'test.jpg'})
-        >>> print(html.stringify_xml(Index().html_latest_post(**kwargs)))
-        <div class="column">
-          <h2>Latest Post</h2>
-          <a href="test.html">
-            <h3 class="title">Test</h3>
-          </a>
-          <figure>
-            <a href="test.html">
-              <img src="test.jpg">
-            </a>
-            <figcaption>
-              <p>a test</p>
-            </figcaption>
-          </figure>
-        </div>
-        """
-        column = html.column()
-        column.append(html.h2(text='Latest Post'))
-
-        column.append(
-            html.link(href=page_href, children=[html.h3(_class='title', text=title)]))
-
-        if banner_href:
-            column.append(
-                html.figure(src=banner_href,
-                            href=page_href,
-                            caption=description))
-        else:
-            column.append(html.p(text=description))
-
-        return column
-
-    def html_last_updated(self, commit=None, timestamp=None):
-        """Render last update column.
-
-        >>> commit = git.Commit(short_hash='abc', long_hash='abcdefg', summary='Fake Commit', url='git.biz/abcdefg')
-        >>> timestamp = 'Friday, February 04 2022 9:18 AM CST'
-        >>> xml = Index().html_last_updated(commit=commit, timestamp=timestamp)
-        >>> print(html.stringify_xml(xml))
-        <div class="column">
-          <h2>Last Updated</h2>
-          <p>
-            <small class="code">[<a href="git.biz/abcdefg">abc</a>]<br>Fake Commit</small>
-            <br>
-            <small>Friday, February 04 2022 9:18 AM CST</small>
-          </p>
-        </div>
-        """
-        column = html.column()
-        column.append(html.h2(text='Last Updated'))
-        column.append(html.p(children=[
-            html.small(_class='code', children=[
-                '[',
-                html.link(href=commit.url, text=commit.short_hash),
-                ']',
-                html.br(),
-                commit.summary,
-            ]),
-            html.br(),
-            html.small(text=timestamp),
-        ]))
-        
-        return column
-
-    def make_timestamp(self):
-        timestamp = datetime.datetime.now()
-        timestamp_format = '%A, %B %d %Y %-I:%M %p'
-        if zone := timestamp.tzname():
-            timestamp_format += f' {zone}'
-        else:
-            timestamp_format += ' CST'
-
-        return timestamp.strftime(timestamp_format)
-
-    def fix_self_closing_tags(self, html: str):
-        """Adds extra slash to self-closing tags.
-
-        Unfortunately this is needed to satisfy the XML library's
-        parser while reading data.
-
-        >>> html = '<div><img src="bleh.jpg"></div>'
-        >>> Index().fix_self_closing_tags(html)
-        '<div><img src="bleh.jpg"/></div>'
-        """
-        p_img = re.compile(r'\<img src=\"(.*)\"\>')
-        html =  p_img.sub(r'<img src="\1"/>', html)
-
-        p_br = re.compile(r'\<br\>')
-        html =  p_br.sub('<br/>', html)
-        return html
-
-    def fetch_latest_entry(self):
-        """Return latest journal entry."""
-
-        entries = utils.ROOT_DIR.glob('entries/*.html')
-        entries = sorted(entries, reverse=True)
-        latest = Entry(source=entries[0])
-
-        # TODO: this is kind of gross, innit?  But we don't need the
-        # pagination just to display the latest entry on the homepage,
-        # so stub it out.
-        latest.paginate()
-
-        return latest
-
-
 def main(args):
-    index = Index()
-    nav_pages = build_nav_list()
-    index.build(author=args.author,
-                year=args.year,
-                full_url=args.full_url,
-                nav_pages=nav_pages,
-                expand_macros=False)
-    logger.info('generated index page %s', index)
+    document = Document(
+        filename='index.html',
+        title=args.title,
+        description=args.subtitle,
+        author=args.author,
+        nav_pages=['entries.html', 'pets.html', 'contact.html'],
+    )
+
+    with open(document.target, 'w') as f:
+        f.write(document.render())
+    logger.info('generated %s', document.filename)
--- src/document.py
+++ src/document.py
@@ -1,27 +1,66 @@
+import datetime
+import pathlib
 import string
+import xml.etree.ElementTree
 
 
 class Document:
-    def __init__(self, title='', description='', url='', banner_url=''):
-        self.title = title
-        self.description = description
-        self.url = url
+    def __init__(
+            self,
+            author='',
+            banner_filename='',
+            banner_url='',
+            content='',
+            description='',
+            filename='',
+            nav_pages=[],
+            page_next=None,
+            page_previous=None,
+            title='',
+            url='',
+            year=datetime.datetime.now().year,
+        ):
+        self.author = author
+        self.banner_filename = banner_filename
         self.banner_url = banner_url
+        self.content = content
+        self.description = description
+        self.filename = filename
+        self.nav_pages = nav_pages
+        self.page_next = page_next
+        self.page_previous = page_previous
+        self.title = title
+        self.url = url
+        self.year = year
+
+    @property
+    def target(self) -> str:
+        """Absolute path to the target file"""
+
+        here = pathlib.Path(__file__).parent.absolute()
+        root = here.parent
+        webroot = root / 'www'
+
+        assert self.filename, "Can't render target unless filename is set!"
+        return str(webroot / self.filename)
+        
 
     def render(self) -> str:
         """Render the document as a string."""
 
         head = self.render_head()
         body = self.render_body()
-        output = """
-<!doctype html>
+        
+        output = f"""
 <html lang="en">
-${head}
-${body}
+{head}
+{body}
 </html>
 """.strip()
 
-        return output
+        output = prettify_html(output)
+
+        return f"<!doctype html>\n{output}"
 
     def render_head(self) -> str:
         """Render document <head>"""
@@ -29,18 +68,18 @@ ${body}
         template = """
 <head>
   <title>${title}</title>
-  <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
-  <link href="./assets/site.css" rel="stylesheet">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="twitter:title" content="${title}">
-  <meta name="twitter:description" content="${description}">
-  <meta property="og:url" content="${url}">
-  <meta property="og:type" content="article">
-  <meta property="og:title" content="${title}">
-  <meta property="og:description" content="${description}">
-  <meta name="image" content="${banner_url}">
-  <meta property="og:image" content="${banner_url}">
+  <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico"/>
+  <link href="./assets/site.css" rel="stylesheet"/>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <meta name="twitter:title" content="${title}"/>
+  <meta name="twitter:description" content="${description}"/>
+  <meta property="og:url" content="${url}"/>
+  <meta property="og:type" content="article"/>
+  <meta property="og:title" content="${title}"/>
+  <meta property="og:description" content="${description}"/>
+  <meta name="image" content="${banner_url}"/>
+  <meta property="og:image" content="${banner_url}"/>
 </head>
 """.strip()
 
@@ -55,35 +94,108 @@ ${body}
     def render_body(self) -> str:
         """Render document <body>"""
 
-        template = """
-  <body>
-    <header>
-      <h1>Monday, January 3 2022</h1>
-      <h2>the ball drop, exercise, and resolutions</h2>
-    </header>
-    <hr>
-    <nav>
-      <a href="/">index.html</a>
-      <span>/</span>
-      <span>2022-01-03.html</span>
-      <br class="show-on-mobile">
-      <span class="float-right-on-desktop">
-        <a href="/entries.html">entries.html</a>
-        <a href="/pets.html">pets.html</a>
-        <a href="/contact.html">contact.html</a>
-      </span>
-    </nav>
-    <hr>
-    <figure>
-      <a href="./images/banners/2022-01-03.jpg">
-        <img alt="banner" src="./images/banners/2022-01-03.jpg">
-      </a>
-    </figure>
-    <article>
-    </article>
-    <hr>
-    <footer>
-      <small>© Copyright 2022 Alex Recker</small>
-    </footer>
-  </body>
+        body = f"""
+{self.render_body_header()}
+<hr/>
+{self.render_body_nav()}
+<hr/>
+{self.render_body_banner()}
+{self.render_body_article()}
+<hr/>
+{self.render_body_footer()}
 """.strip()
+
+        body = '\n'.join(['  ' + line for line in body.splitlines()])
+        body = '<body>\n' + body + '\n</body>'
+        return body
+
+    def render_body_header(self):
+        return f"""
+<header>
+  <h1>{self.title}</h1>
+  <h2>{self.description}</h2>
+</header>
+""".strip()
+
+    def render_body_nav(self):
+        """Render page nav bar"""
+
+        breadcrumbs = f'  <a href="./{self.filename}">{self.filename}</a>'
+
+        if self.filename != 'index.html':
+            breadcrumbs += '\n  <span>/</span>'
+            breadcrumbs += '\n  <span>{self.filename}</span>'
+
+        navlist = '\n'.join([f'  <a href="./{f}">{f}</a>' for f in self.nav_pages])
+
+        return f"""
+<nav>
+{breadcrumbs}
+<br class="show-on-mobile" />
+<span class="float-right-on-desktop">
+{navlist}
+</span>
+</nav>
+""".strip()
+
+    def render_body_banner(self) -> str:
+        """Render page banner (if one was given)."""
+
+        if not self.banner_filename:
+            return ""
+
+        return f"""
+<figure>
+  <a href="./{self.banner_filename}">
+    <img alt="banner" src="./{self.banner_filename}">
+  </a>
+</figure>
+""".strip()
+
+    def render_body_article(self) -> str:
+        """Render page <article>.
+
+        Returns Document().content if it's set, otherwise will try to
+        build it dynamically from rows and columns.
+        """
+        if self.content:
+            content = self.content
+        else:
+            # TODO: Render from rows, columns, and objects?
+            content = ''
+
+        pages = []
+        if self.page_previous:
+            pages.append(f'  <a class="float-left" href="./{self.page_previous}">⟵ {self.page_previous}</a>')
+        if self.page_next:
+            pages.append(f'  <a class="float-right" href="./{self.page_next}">{self.page_next} ⟶</a>')
+
+        if pages:
+            content += '\n<nav class="clearfix">\n' + '\n'.join(pages) + '\n</nav>'
+
+        return f'<article>\n{content}\n</article>'
+
+    def render_body_footer(self) -> str:
+        """Render page <footer>"""
+
+        return f"""
+<footer>
+  <small>© Copyright {self.year} {self.author}</small>
+</footer>
+""".strip()
+
+
+def prettify_html(html: str) -> str:
+    """Prettify an HTML string."""
+
+    # Need to use a custom target so we read comments too.
+    target = xml.etree.ElementTree.TreeBuilder(insert_comments=True)
+    parser = xml.etree.ElementTree.XMLParser(target=target)
+
+    try:
+        parser.feed(html)
+        html = parser.close()
+        xml.etree.ElementTree.indent(html)
+        return xml.etree.ElementTree.tostring(html, encoding='unicode', method='html')
+    except xml.etree.ElementTree.ParseError as e:
+        raise ValueError(f'{e}\n---\n{html}\n---')
