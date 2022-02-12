1407c2aac6fdcc5272183eb14719d239411680db
Alex Recker <alex@reckerfamily.com>
2022-02-12 14:34:29 -0600

    Finish archives rewrite

--- src/commands/archives.py
+++ src/commands/archives.py
@@ -1,111 +1,144 @@
 """generate journal archives"""
-import random
-import logging
 
-from .. import utils, html
-from .pages import Page, build_nav_list
+import logging
+import random
+import urllib.parse
+
+from .. import utils
 
 logger = logging.getLogger(__name__)
 
 
-class Archive:
-    def __init__(self, site=None, entries=[], full_url=''):
-        self.site = site
-        self.entries = entries
-
-        if full_url:
-            self.protocol = full_url.scheme
-            self.domain = full_url.netloc
-
-    def __repr__(self):
-        path = utils.prettify_path(utils.ROOT_DIR / 'entries/')
-        return f'<Archive {path}>'
-
-    def list_years(self):
-        years = [entry.date.year for entry in self.entries]
-        return sorted(set(years), reverse=True)
-
-    def list_entries(self, year='', month=''):
-        entries = (e for e in self.entries)
-        if year:
-            entries = filter(lambda e: year == e.date.year, entries)
-        if month:
-            entries = filter(lambda e: month == e.date.month, entries)
-        return sorted(entries, key=lambda e: e.date, reverse=True)
-
-    def pages(self):
-        years = self.list_years()
-
-        page = Page(filename='entries.html',
-                    title='Entries',
-                    description='Complete Archive of Journal Entries',
-                    banner=self.pick_banner(),
-                    is_entry=False,
-                    content=self.build_page_content(year=None, years=years),
-                    site=self.site)
-        yield page
-
-        for year in years:
-            page = Page(filename=f'{year}.html',
-                        title=str(year),
-                        description=f'All Entries from {year}',
-                        banner=self.pick_banner(year=year),
-                        is_entry=False,
-                        content=self.build_page_content(year=year,
-                                                        years=years),
-                        site=self.site)
-            yield page
-
-    def build_page_content(self, year=None, years=[]):
-        root = html.p()
-        nav_pages = [f'{y}.html' for y in years]
-        nav = html._nav()
-        nav.append(html.build_page_nav(nav_pages=nav_pages))
-        root.append(nav)
-        content = html.stringify_xml(root)
-
-        if year:
-            rows = [[f'./{e.filename}', e.description]
-                    for e in self.list_entries(year=year)]
-            table = html.build_link_table(rows=rows)
-            table = html.stringify_xml(table)
-            content = content + '\n' + table
-
-        content = f'''
-<p>
-  <span>
-    <a href="https://validator.w3.org/feed/check.cgi?url={self.protocol}%3A//{self.domain}/feed.xml">
-      <img src="/assets/valid-atom.png" alt="[Valid Atom 1.0]" title="Validate my Atom 1.0 feed" />
-    </a>
-  </span>
-  <br/>
-  RSS feed available here: <a href="/feed.xml">feed.xml</a>
-</p>
-'''.strip() + '\n' + content
-
-        return content
-
-    def pick_banner(self, year=''):
-        entries = self.list_entries(year=year)
-        choices = filter(None, (e.banner_filename() for e in entries))
-        try:
-            return random.choice(list(choices))
-        except IndexError:
-            return None
+def regiser(parser):
+    return parser
 
 
-def main(args):
-    # TODO: remove once Site is gone
-    from ..models import Site
-    site = Site(**vars(args))
+def render_feed_link(html: utils.StringWriter,
+                     full_url: urllib.parse.ParseResult):
+    html.comment('RSS Feed')
+    with html.block('p', blank=True):
+        link = full_url.geturl()
+        link = urllib.parse.urljoin(link, 'feed.xml')
+        link = urllib.parse.quote_plus(link)
+        link = f'https://validator.w3.org/feed/check.cgi?url={link}'
+        with html.block('a', href=link):
+            src = './assets/valid-atom.png'
+            title = 'Validate my Atom 1.0 feed'
+            alt = '[Valid Atom 1.0]'
+            html.write(f'<img src="{src}" alt="{alt}" title="{title}">')
 
-    archive = Archive(site=site, entries=site.entries, full_url=args.full_url)
-    pages = list(archive.pages())
-    total = len(pages)
-    nav_pages = build_nav_list()
-    for i, page in enumerate(pages):
-        page.build(author=args.author,
-                   year=args.year,
-                   full_url=args.full_url,
-                   nav_pages=nav_pages)
-        logger.info('generated archive page %s (%d/%d)', page, i + 1, total)
+    with html.block('p', blank=True):
+        html.write(
+            'RSS feed available here: <a href="./feed.xml">feed.xml</a>')
+
+    return html
+
+
+def render_years_nav(html: utils.StringWriter, years=[]):
+    html.comment('Year Archives')
+    with html.block('nav', blank=True):
+        with html.block('span'):
+            for y in sorted(years, reverse=True):
+                html.write(f'<a href="./{y}.html">{y}.html</a>')
+    return html
+
+
+def render_banner(html: utils.StringWriter, choice: utils.Entry):
+    html.comment('Random Banner')
+    link = f'Taken from <a href="./{choice.filename}">{choice.description}</a>'
+    html.figure(src=f'./images/banners/{choice.banner}', caption=link)
+    return html
+
+
+def render_entries_table(html: utils.StringWriter, entries=[]):
+    with html.block('table'):
+        for entry in entries:
+            with html.block('tr'):
+                with html.block('td'):
+                    html.write(
+                        f'<a href="./{entry.filename}">{entry.filename}</a>')
+                html.write(f'<td>{entry.description}</td>')
+
+    return html
+
+
+def render_entries_page(entries=[],
+                        full_url=None,
+                        nav=[],
+                        year=None,
+                        author='',
+                        years=[]):
+
+    html = utils.StringWriter(starting_indent=4)
+    html = render_feed_link(html, full_url)
+    html = render_years_nav(html, years=years)
+    choice = random.choice([e for e in entries if e.banner])
+    html = render_banner(html, choice)
+
+    page = utils.Page('entries.html', 'Entries',
+                      'Complete Archive of Journal Entries', None)
+    return utils.render_page(page,
+                             full_url,
+                             content=html.text.rstrip(),
+                             nav_pages=nav,
+                             year=year,
+                             author=author)
+
+
+def render_year_page(year=None,
+                     entries=[],
+                     full_url=None,
+                     nav=[],
+                     copyright_year=None,
+                     author='',
+                     years=[]):
+    html = utils.StringWriter(starting_indent=4)
+    html = render_feed_link(html, full_url)
+    html = render_years_nav(html, years=years)
+
+    try:
+        choice = random.choice([e for e in entries if e.banner])
+        html = render_banner(html, choice)
+    except IndexError:
+        pass
+
+    html = render_entries_table(html, entries)
+    page = utils.Page(f'{year}.html', str(year), f'All Entries from {year}',
+                      None)
+    return utils.render_page(page,
+                             full_url,
+                             content=html.text.rstrip(),
+                             nav_pages=nav,
+                             year=copyright_year,
+                             author=author)
+
+
+def main(args, entries=[], nav=[]):
+    nav = nav or utils.read_nav(args.directory / 'data')
+    entries = entries or utils.fetch_entries(args.directory / 'entries')
+    years = set([e.date.year for e in entries])
+
+    output = render_entries_page(entries=entries,
+                                 full_url=args.full_url,
+                                 nav=nav,
+                                 year=args.year,
+                                 author=args.author,
+                                 years=years)
+    with open(args.directory / 'www/entries.html', 'w') as f:
+        f.write(output)
+    logger.info('generated entries.html')
+
+    for year in set([e.date.year for e in entries]):
+        year_entries = [e for e in entries if e.date.year == year]
+        output = render_year_page(
+            year=year,
+            entries=year_entries,
+            full_url=args.full_url,
+            nav=nav,
+            copyright_year=args.year,
+            author=args.author,
+            years=years,
+        )
+        with open(args.directory / f'www/{year}.html', 'w') as f:
+            f.write(output)
+        logger.info('generated %s', f'{year}.html')
