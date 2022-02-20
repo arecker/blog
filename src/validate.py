14c171b6f565abcbcef380bd6834d4f1fd912575
Alex Recker <alex@reckerfamily.com>
2022-02-20 10:50:44 -0600

    Remove validate command

--- src/validate.py
+++
@@ -1,9 +0,0 @@
-"""validate site files"""
-
-import logging
-
-logger = logging.getLogger(__name__)
-
-def main(args):
-    pages = sorted(list(args.directory.glob('www/*.html')))
-    assert pages, 'No pages found!  Build the project first.'
