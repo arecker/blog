f6273b3a7a90078fb9defb039812a13e98e5c6cd
Alex Recker <alex@reckerfamily.com>
2022-03-28 10:33:20 -0500

    Logging in main

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,13 @@
+import logging
+import platform
+import sys
+
+logger = logging.getLogger(__name__)
+
+
 def main():
     """Run main CLI routine."""
 
-    print('main')
+    logging.basicConfig(stream=sys.stderr, format='%(name)s: %(message)s', level=logging.DEBUG)
+
+    logger.debug('running blog with python v%s', platform.python_version())
