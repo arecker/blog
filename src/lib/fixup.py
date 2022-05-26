5b5ae15685aa04b873dc52194380860d7c030c50
Alex Recker <alex@reckerfamily.com>
2022-05-25 21:22:01 -0500

    Delete unused images

--- src/lib/fixup.py
+++ src/lib/fixup.py
@@ -1,10 +1,12 @@
 import logging
 import pathlib
 
+from .images import scan_images, fetch_images
+
 logger = logging.getLogger(__name__)
 
 
-def fixup_project(entries_dir=''):
+def fixup_project(entries_dir='', www_dir=''):
     entries_dir = pathlib.Path(entries_dir)
     entries = list(entries_dir.glob('*.html'))
     total = len(entries)
@@ -15,5 +17,19 @@ def fixup_project(entries_dir=''):
             content = f.read()
             if '--> <!--' in content:
                 logger.warning('%s: found misaligned metadata', entry.name)
+
         if i % 100 == 0:
             logger.info('scanned entries %d of %d', i, total)
+
+    for image in fetch_images(www_directory=www_dir):
+        found = False
+        for entry in entries:
+            with entry.open('r') as f:
+                if image.name in f.read():
+                    found = True
+                    break
+        if not found:
+            image.unlink()
+            logger.warn('deleted unused image %s', image)
+
+    # scan_images(dir_www=www_dir)
